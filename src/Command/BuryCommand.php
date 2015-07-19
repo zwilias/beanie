a52acd1647c13d0b9f0fbfee1dba218998efaa6e
Ilias <ilias@idlemonkeys.net>
2015-07-19 23:19:26 +0200

    Implement BURY command

---
+++ src/Command/BuryCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Beanie\Command;
+
+
+use Beanie\Beanie;
+use Beanie\Command;
+use Beanie\Exception\NotFoundException;
+use Beanie\Exception\UnexpectedResponseException;
+use Beanie\Response;
+use Beanie\Server\Server;
+
+class BuryCommand extends AbstractCommand
+{
+    /** @var int */
+    protected $_jobId;
+
+    /** @var int */
+    protected $_priority;
+
+    /**
+     * @param int $jobId
+     * @param int $priority
+     */
+    public function __construct($jobId, $priority = Beanie::DEFAULT_PRIORITY)
+    {
+        $this->_jobId = (int)$jobId;
+        $this->_priority = (int)$priority;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws NotFoundException
+     * @throws UnexpectedResponseException
+     */
+    protected function _parseResponse($responseLine, Server $server)
+    {
+        switch ($responseLine) {
+            case Response::FAILURE_NOT_FOUND:
+                throw new NotFoundException($this, $server);
+            case Response::RESPONSE_BURIED:
+                return new Response($responseLine, null, $server);
+            default:
+                throw new UnexpectedResponseException($responseLine, $this, $server);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function getCommandLine()
+    {
+        return join(' ', [
+            Command::COMMAND_BURY,
+            $this->_jobId,
+            $this->_priority
+        ]);
+    }
+}
---
+++ tests/Beanie/Command/BuryCommandTest.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace Beanie\Command;
+
+use Beanie\Beanie;
+use Beanie\Command;
+use Beanie\Response;
+
+require_once 'WithServerMock_TestCase.php';
+
+class BuryCommandTest extends WithServerMock_TestCase
+{
+    const TEST_ID = 999;
+
+    public function testGetCommandLine_noArgs_usesDefault()
+    {
+        $buryCommand = new BuryCommand(self::TEST_ID);
+        $expected = join(' ', [
+            Command::COMMAND_BURY,
+            self::TEST_ID,
+            Beanie::DEFAULT_PRIORITY
+        ]);
+
+
+        $this->assertEquals($expected, $buryCommand->getCommandLine());
+    }
+
+    public function testGetCommandLine_withArgs_usesArgs()
+    {
+        $priority = 8888;
+        $expected = join(' ', [
+            Command::COMMAND_BURY,
+            self::TEST_ID,
+            $priority
+        ]);
+
+        $buryCommand = new BuryCommand(self::TEST_ID, $priority);
+
+
+        $this->assertEquals($expected, $buryCommand->getCommandLine());
+    }
+
+    /**
+     * @expectedException \Beanie\Exception\NotFoundException
+     * @expectedExceptionCode 404
+     */
+    public function testParseResponse_notFoundFailure_throwsException()
+    {
+        (new BuryCommand(self::TEST_ID))->parseResponse(Response::FAILURE_NOT_FOUND, $this->_getServerMock());
+    }
+
+    /**
+     * @expectedException \Beanie\Exception\UnexpectedResponseException
+     * @expectedException 402
+     */
+    public function testParseResponse_unexpectedResponse_throwsException()
+    {
+        (new BuryCommand(self::TEST_ID))->parseResponse('WTF', $this->_getServerMock());
+    }
+
+    public function testParseResponse_buriedResponse_returnsResponse()
+    {
+        $response = (new BuryCommand(self::TEST_ID))->parseResponse(Response::RESPONSE_BURIED, $this->_getServerMock());
+
+
+        $this->assertInstanceOf('\Beanie\Response', $response);
+        $this->assertEquals(Response::RESPONSE_BURIED, $response->getName());
+        $this->assertNull($response->getData());
+        $this->assertEquals($this->_getServerMock(), $response->getServer());
+    }
+}
