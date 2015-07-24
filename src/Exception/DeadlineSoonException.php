6d496054e283dda554010eb7a9272c0b465de7df
Ilias <ilias@idlemonkeys.net>
2015-07-24 02:29:28 +0200

    Use positional params for sprintf

--- src/Exception/DeadlineSoonException.php
+++ src/Exception/DeadlineSoonException.php
@@ -6,18 +6,9 @@ namespace Beanie\Exception;
 
 use Beanie\Command;
 use Beanie\Exception;
-use Beanie\Server\Server;
 
-class DeadlineSoonException extends Exception
+class DeadlineSoonException extends AbstractServerCommandException
 {
     const DEFAULT_CODE = 408;
-    const DEFAULT_MESSAGE = 'There is a deadline for a running job approaching soon on \'%s\'. \'%s\' failed.';
-
-    public function __construct(Command $command, Server $server)
-    {
-        parent::__construct(
-            sprintf(self::DEFAULT_MESSAGE, (string) $server, get_class($command)),
-            self::DEFAULT_CODE
-        );
-    }
+    const DEFAULT_MESSAGE = 'There is a deadline for a running job approaching soon on \'%2$s\'. \'%1$s\' failed.';
 }
